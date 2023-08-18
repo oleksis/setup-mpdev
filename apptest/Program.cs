fb4a33196013e7c469ec2e787c7c48cd4731cdaf
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-08-18 10:49:41 -0400

    Add .Net console app

---
+++ apptest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace apptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Figgle.FiggleFonts.Standard.Render("Hello, Master Packager Dev!"));
+        }
+    }
+}
